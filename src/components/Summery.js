ab5b86b8a3574e6ac83f304e3c1a3884841dee67
shaheen2013 <coredeveloper.2013@gmail.com>
2022-11-08 19:34:56 +0600

    all pages converted to react

---
+++ src/components/Summery.js
@@ -0,0 +1,21 @@
+import image from "../assets/images/3.jpg";
+import classes from "../styles/Summery.module.css";
+
+export default function Summery() {
+  return (
+    <>
+      <div className={classes.summary}>
+        <div className={classes.point}>
+          {/* <!-- progress bar will be placed here --> */}
+          <p className={classes.score}>
+            Your score is <br />5 out of 10
+          </p>
+        </div>
+
+        <div className={classes.badge}>
+          <img src={image} alt="Success" />
+        </div>
+      </div>
+    </>
+  );
+}
